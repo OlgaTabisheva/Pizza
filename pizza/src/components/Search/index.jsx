81d0ad4d25c80c56ab6a5e2a125166de6682398e
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2022-11-25 17:14:54 +0300

    correct

--- pizza/src/components/Search/index.jsx
+++ pizza/src/components/Search/index.jsx
@@ -46,7 +46,9 @@ const Search  = () =>{
         onChange={(ev)=>setSearchValue(ev.target.value)}
         className={styles.input}
         placeholder="Поиск пиццы..."/>
-      {searchValue && (<svg className={styles.clearIcon}
+      {searchValue && (<svg
+          onClick={()=>setSearchValue('')}
+          className={styles.clearIcon}
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg">
           <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
